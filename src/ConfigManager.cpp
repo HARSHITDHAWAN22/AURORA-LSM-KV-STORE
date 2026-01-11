5659fce11320d5eb7eac9f35684ca1d8fdcab233
HARSHITDHAWAN22 <harshitdhawan2202@gmail.com>
2026-01-11 08:58:04 +0530

    Fix JSON include and add nlohmann json header

---
+++ src/ConfigManager.cpp
@@ -0,0 +1,63 @@
+#include "ConfigManager.h"
+#include<fstream>
+#include<iostream>
+#include<nlohmann/json.hpp>
+using namespace std;
+
+using json = nlohmann::json;
+
+ConfigManager::ConfigManager(const std::string& configPath)
+    : configFilePath(configPath),
+      memTableMaxEntries(0),
+      bloomFilterBitSize(0),
+      bloomFilterHashCount(0),
+      maxFilesPerLevel(0) {}
+
+bool ConfigManager::load(){
+    ifstream file(configFilePath);
+
+    if(!file.is_open()){
+        std::cerr<<"Failed to open config file: "<< configFilePath << std::endl;
+        return false;
+    }
+
+    json config;
+    file >> config;
+
+    memTableMaxEntries =
+        config["storage"]["memtable"]["max_entries"];
+
+    sstableDirectory =
+        config["storage"]["sstable"]["data_directory"];
+
+    bloomFilterBitSize =
+        config["bloom_filter"]["bit_size"];
+
+    bloomFilterHashCount =
+        config["bloom_filter"]["hash_functions"];
+
+    maxFilesPerLevel =
+        config["compaction"]["max_files_per_level"];
+
+    return true;
+}
+
+int ConfigManager::getMemTableMaxEntries() const {
+    return memTableMaxEntries;
+}
+
+int ConfigManager::getBloomFilterBitSize() const {
+    return bloomFilterBitSize;
+}
+
+int ConfigManager::getBloomFilterHashCount() const {
+    return bloomFilterHashCount;
+}
+
+int ConfigManager::getMaxFilesPerLevel() const {
+    return maxFilesPerLevel;
+}
+
+string ConfigManager::getSSTableDirectory() const {
+    return sstableDirectory;
+}
