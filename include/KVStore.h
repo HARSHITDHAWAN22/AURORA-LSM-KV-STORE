0104dc2a7cf9fc674404dfb2c19e4337bca53144
HARSHITDHAWAN22 <harshitdhawan2202@gmail.com>
2026-01-17 23:47:29 +0530

    Flush MemTable to SSTable and enable disk reads

--- include/KVStore.h
+++ include/KVStore.h
@@ -2,8 +2,11 @@
 #define KV_STORE_H
 
 #include<string>
+#include<vector>
+
 #include "ConfigManager.h"
 #include "MemTable.h"
+#include "SSTable.h"
 
 class KVStore{
 public:
@@ -14,10 +17,18 @@ public:
     bool get(const std::string& key, std::string& value);
     void deleteKey(const std::string& key);
 
+    private:
+    void flushMemTable();
+
+
 private:
 
     ConfigManager configManager;
     MemTable memTable;
+
+    std::vector<SSTable> sstables;
+
+    int sstableCounter;
 
 };
 /*Also: putting using namespace std; in a header file forces every file that includes it to import the entire std namespace, which can cause name conflicts and unexpected errors in large projects. */
--- src/KVStore.cpp
+++ src/KVStore.cpp
@@ -1,39 +1,63 @@
 #include "KVStore.h"
-#include<iostream>
+#include <iostream>
 
 KVStore::KVStore(const std::string& configPath)
     : configManager(configPath),
-      memTable(0)   // temporary init
-
+      memTable(0),
+      sstableCounter(0)
 {
-
+    
     if(!configManager.load()){
-        std::cerr<< "Failed to load configuration." << std::endl;
+        std::cerr << "Failed to load configuration.\n";
         return;
     }
 
-
-    // Reinitialize MemTable with config value
     memTable = MemTable(configManager.getMemTableMaxEntries());
-
 }
+
 
 void KVStore::put(const std::string& key, const std::string& value){
     memTable.put(key, value);
 
     if(memTable.isFull()){
-        std::cout << "MemTable full. Flush to disk will be implemented next.\n";
-        memTable.clear();
-
+        flushMemTable();
     }
 }
 
-bool KVStore::get(const std::string& key, std::string& value){
 
-    return memTable.get(key, value);
+bool KVStore::get(const std::string& key, std::string& value){
+    if(memTable.get(key, value)){
+        return true;
+    }
+
+    
+    for(auto it = sstables.rbegin(); it != sstables.rend(); ++it){
+        if(it->get(key, value)){
+            return true;
+        }
+    }
+
+    return false;
 }
 
-void KVStore::deleteKey(const std::string& key){
-    
+void KVStore::deleteKey(const std::string& key) {
     memTable.remove(key);
+
+    if (memTable.isFull()) {
+        flushMemTable();
+    }
+}
+
+void KVStore::flushMemTable() {
+    std::string filePath =
+        configManager.getSSTableDirectory() +
+        "/sstable_" + std::to_string(sstableCounter++) + ".dat";
+
+    SSTable sstable(filePath);
+
+    if (sstable.writeToDisk(memTable.getData())) {
+        sstables.push_back(sstable);
+        memTable.clear();
+        std::cout << "Flushed MemTable to " << filePath << "\n";
+    }
 }
