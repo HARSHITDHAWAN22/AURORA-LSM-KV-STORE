5f7746f38b62a642fab3422a6a0e1d9addd7797f
HARSHITDHAWAN22 <harshitdhawan2202@gmail.com>
2026-03-06 23:26:04 +0530

    Designed and implement LRU cache.Connection with the tables is left.

---
+++ include/LRUCache.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <unordered_map>
+#include <list>
+#include <string>
+
+class LRUCache{
+
+private:
+
+    struct Node{
+        std::string key;
+        std::string value;
+    };
+
+    size_t capacity;
+
+    std::list<Node> cacheList;
+
+    std::unordered_map<std::string, std::list<Node>::iterator> cacheMap;
+
+public:
+
+    size_t hits = 0;
+    size_t misses = 0;
+
+    LRUCache(size_t cap);
+
+    bool get(const std::string& key, std::string& value);
+
+    void put(const std::string& key, const std::string& value);
+
+};
---
+++ src/LRUCache.cpp
@@ -0,0 +1,50 @@
+#include "LRUCache.h"
+
+LRUCache::LRUCache(size_t cap){
+    capacity = cap;
+}
+
+bool LRUCache::get(const std::string& key, std::string& value){
+
+    auto it = cacheMap.find(key);
+
+    if (it == cacheMap.end()) {
+        misses++;
+        return false;
+    }
+
+    hits++;
+
+    cacheList.splice(cacheList.begin(), cacheList, it->second);
+
+    value = it->second->value;
+
+    return true;
+}
+
+void LRUCache::put(const std::string& key, const std::string& value){
+
+    auto it = cacheMap.find(key);
+
+    if (it != cacheMap.end()) {
+
+        it->second->value = value;
+
+        cacheList.splice(cacheList.begin(), cacheList, it->second);
+
+        return;
+    }
+
+    if(cacheList.size() == capacity){
+
+        auto last = cacheList.back();
+
+        cacheMap.erase(last.key);
+
+        cacheList.pop_back();
+    }
+
+    cacheList.push_front({key,value});
+
+    cacheMap[key] = cacheList.begin();
+}
